f11185797d1731d784af96a0d311cd8bd08ab5d7
WEEEEEEEEEEEEEEEEEEEEEEE <thanussian@gmail.com>
2022-03-31 13:56:12 -0400

    added variations

--- pages/index.js
+++ pages/index.js
@@ -1,44 +1,35 @@
-import Head from 'next/head'
-import Image from 'next/image'
-
-import { BookIcon, TwitterIcon, LinkedinIcon, GithubIcon, EmailIcon, ResumeIcon } from '../public/helpers/icons.js';
-
-import Tilt from 'react-tilt'
-
 export default function Home() {
   return (
-    <div className="select-none">
+    <div className="font-['Helvetica'] select-none text-center font-extrabold mt-8">
 
-      <Head>
-        <title>Thanussian Sharvananthan</title>
-        <meta name="description" content="The personal portfolio of Thanussian Sharvananthan" />
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
+      <header>
+        <p className="text-9xl">hey.</p>
+        <p className="text-9xl mt-64">welcome to my site.</p>
+        <p className="text-9xl mt-64">i didn't know how <br /> to design it.</p>
+        <p className="text-9xl mt-64">so i created the same website...</p>
+        <p className="text-9xl mt-64">...multiple times</p>
+      </header>
 
-      <div className="grid place-items-center h-screen">
-        <div>
-          <div className="w-full text-center">
-            <Tilt options={{ max: 30 }}>
-              <Image className="text-center" src="/images/pingu.png" height={350} width={350} alt="NOOT NOOT"/>
-            </Tilt>
-          </div>
+      <p className="text-9xl mt-64"> <a href="/variations/pingu" className="text-blue-500 hover:text-blue-600" target="_blank"> Pingu </a> is a site with <br /> a Pingu icon</p>
+      <p className="text-9xl mt-64">(who woulda known)</p>
 
-          <p className="text-8xl text-center font-extrabold my-6 text-white">Thanussian Sharvananthan</p>
-          <p className="text-3xl text-center font-extrabold my-4 text-white">I Like To Build Stuff :)</p>
+      <p className="text-9xl mt-64"> <a href="/variations/no-name" className="text-blue-500 hover:text-blue-600" target="_blank"> No Name </a> looks like a <br /> no name product</p>
+      <p className="text-9xl mt-64">(who also woulda known)</p>
+      <p className="text-9xl mt-64">just kidding</p>
+      <p className="text-9xl mt-64">can't leave out americans</p>
 
-          <div className="border-t border-2 border-white my-8"></div>
+      <div className="border-t border-2 border-black mt-32"></div>
+      <p className="text-9xl mt-32">more layouts coming <br /> soon</p>
+      <p className="text-9xl mt-64">(that's a lie)</p>
+      <p className="text-9xl mt-64">also</p>
+      <p className="text-9xl mt-64">some layouts aren't <br /> responsive</p>
+      <p className="text-9xl mt-64">(by "some", i mean <i>most</i>)</p>
 
-          <div className="flex justify-around">
-            <a href="https://adcap.vercel.app" target="_blank" rel="noreferrer"> <BookIcon /> </a>
-            <a href="https://twitter.com/Than_Sharva" target="_blank" rel="noreferrer"> <TwitterIcon /> </a>
-            <a href="https://www.linkedin.com/in/tsharva" target="_blank" rel="noreferrer"> <LinkedinIcon /> </a>
-            <a href="https://github.com/TSharvananthan" target="_blank" rel="noreferrer"> <GithubIcon /> </a>
-            <a href="mailto:thanussian@gmail.com" target="_blank" rel="noreferrer"> <EmailIcon /> </a>
-            <a href="" target="_blank" rel="noreferrer"> <ResumeIcon /> </a>
-          </div>
-        </div>
-      </div>
-
+      <footer className="mb-8">
+        <div className="border-t border-2 border-black mt-32"></div>
+        <p className="text-9xl mt-16">made by <a className="text-red-500 hover:text-red-600" href="https://github.com/TSharvananthan"> ME!!!!!!!!!! </a> </p>
+        <p className="text-4xl mt-16">(all those exclamation marks must mean that i have to be really important)</p>
+      </footer>
     </div>
   )
 }
