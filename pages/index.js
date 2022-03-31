32a323968071bb00f96bed05702af8df75e7c23e
WEEEEEEEEEEEEEEEEEEEEEEE <thanussian@gmail.com>
2022-03-31 14:05:22 -0400

    built portfolio

--- pages/index.js
+++ pages/index.js
@@ -5,7 +5,7 @@ export default function Home() {
       <header>
         <p className="text-9xl">hey.</p>
         <p className="text-9xl mt-64">welcome to my site.</p>
-        <p className="text-9xl mt-64">i didn't know how <br /> to design it.</p>
+        <p className="text-9xl mt-64">i didn&apos;t know how <br /> to design it.</p>
         <p className="text-9xl mt-64">so i created the same website...</p>
         <p className="text-9xl mt-64">...multiple times</p>
       </header>
@@ -16,19 +16,18 @@ export default function Home() {
       <p className="text-9xl mt-64"> <a href="/variations/no-name" className="text-blue-500 hover:text-blue-600" target="_blank"> No Name </a> looks like a <br /> no name product</p>
       <p className="text-9xl mt-64">(who also woulda known)</p>
       <p className="text-9xl mt-64">just kidding</p>
-      <p className="text-9xl mt-64">can't leave out americans</p>
+      <p className="text-9xl mt-64">can&apos;t leave out americans</p>
 
       <div className="border-t border-2 border-black mt-32"></div>
       <p className="text-9xl mt-32">more layouts coming <br /> soon</p>
-      <p className="text-9xl mt-64">(that's a lie)</p>
+      <p className="text-9xl mt-64">(that&apos;s a lie)</p>
       <p className="text-9xl mt-64">also</p>
-      <p className="text-9xl mt-64">some layouts aren't <br /> responsive</p>
-      <p className="text-9xl mt-64">(by "some", i mean <i>most</i>)</p>
+      <p className="text-9xl mt-64">some layouts aren&apos;t <br /> responsive</p>
+      <p className="text-9xl mt-64">(by &quot;some&quot;, i mean <i>most</i>)</p>
 
       <footer className="mb-8">
         <div className="border-t border-2 border-black mt-32"></div>
-        <p className="text-9xl mt-16">made by <a className="text-red-500 hover:text-red-600" href="https://github.com/TSharvananthan"> ME!!!!!!!!!! </a> </p>
-        <p className="text-4xl mt-16">(all those exclamation marks must mean that i have to be really important)</p>
+        <p className="text-9xl mt-16">made by <a className="text-red-500 hover:text-red-600" href="https://github.com/TSharvananthan">than</a> </p>
       </footer>
     </div>
   )
