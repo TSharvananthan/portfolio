52be5c89cecccf9c12b3816daa0fd3c02ab7f403
TSharvananthan <thanussian@gmail.com>
2022-07-11 11:49:22 -0400

    Added resume
    I forgot to include this from the start. I can't lie... I made a quick portfolio that I was proud of and moved on without including the resume.
    
    I KNOW for a fact I told myself something along the lines of 'eh it's whatever, I will upload my resume'

--- pages/index.js
+++ pages/index.js
@@ -50,7 +50,7 @@ const Icons = () => {
         <svg className="text-white" width="48" height="48" viewBox="0 0 24 24" fill="none" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M16 12a4 4 0 10-8 0 4 4 0 008 0zm0 0v1.5a2.5 2.5 0 005 0V12a9 9 0 10-9 9m4.5-1.206a8.959 8.959 0 01-4.5 1.207"/></svg>
       </a>
 
-      <a href="" target="_blank" rel="noreferrer">
+      <a href="/resume.pdf" target="_blank" rel="noreferrer">
         <svg className="text-white" width="48" height="48" viewBox="0 0 24 24"  fill="none"  stroke="currentColor"  strokeWidth="2"  strokeLinecap="round"  strokeLinejoin="round">  <path d="M14 2H6a2 2 0 0 0-2 2v16a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V8z" />  <polyline points="14 2 14 8 20 8" />  <line x1="16" y1="13" x2="8" y2="13" />  <line x1="16" y1="17" x2="8" y2="17" />  <polyline points="10 9 9 9 8 9" /></svg>
       </a>
     </div>
